eef404e12b7cff062691965c0ecb9c68300c404d
BenjaC <benjamincamposv2004@gmail.com>
2025-09-30 20:46:38 -0300

    feat: actualizar Nuevo_Ordenamiento.py desde SubaruDev

--- Nuevo_Ordenamiento.py
+++ Nuevo_Ordenamiento.py
@@ -1,4 +1,4 @@
-def procesar_pares(nombre_archivo, salida_txt):
+def procesar_pares(nombre_archivo):
     lista1 = []  # pares que aparecen una vez
     lista2 = []  # pares que aparecen dos veces
 
@@ -23,21 +23,17 @@ def procesar_pares(nombre_archivo, salida_txt):
             elif par not in lista2 and par_inv not in lista2:
                 lista1.append(par)
 
-    # Escribir resultados en archivo de salida
-    with open(salida_txt, "w") as f_out:
-        for par in lista1:
-            a, b = par
-            f_out.write(f"{a}\t{b}\t1\n")
-            f_out.write(f"{b}\t{a}\t2\n")
+    # Imprimir resultados
+    for par in lista1:
+        a, b = par
+        print(a, b, 1)
+        print(b, a, 2)
 
-        for par in lista2:
-            a, b = par
-            f_out.write(f"{a}\t{b}\t3\n")
-            f_out.write(f"{b}\t{a}\t3\n")
+    for par in lista2:
+        a, b = par
+        print(a, b, 3)
+        print(b, a, 3)
 
 
-# Rutas de entrada y salida
-entrada = r"C:\Users\xxx\Desktop\xxx\ConversionDeMatrices\networks\manrix.txt"
-salida = r"C:\Users\xxx\Desktop\xxx\ConversionDeMatrices\networks\resultado.txt"
-
-procesar_pares(entrada, salida)
+ruta = r"C:\Users\benja\Desktop\ManrixPack\ConversionDeMatrices\networks\manrix.txt"
+procesar_pares(ruta)
