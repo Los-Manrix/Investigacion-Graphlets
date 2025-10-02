abb36805730c801335f0ccc39c3764f89504385f
Benjamin Cruz <bcruze@correo.uss.cl>
2025-10-01 22:38:12 -0300

    fix: correcion de bugs importantes en la lectura de archivos tsv

--- convercionFormato/convercion_formato.py
+++ convercionFormato/convercion_formato.py
@@ -1,64 +1,67 @@
 import os
+import csv
 
 n_nodos = 0
 arcos_a = []
 
 
-def index(file: str, index: str) -> None:
+def index(file: str, index_file: str) -> dict:
     global n_nodos
-    global arcos
     global arcos_a
 
     dic = {}
-    with open (file, 'r') as f:
+    abc_arcos = []
+
+    with open(file, 'r') as f:
         contenido = f.read()
-        abc_arcos = []
+        f.seek(0)
+        lector = csv.reader(f, delimiter='\t')
+
         abc_nodos = contenido.split()
         set_abc = set(abc_nodos)
         counter = 1
 
-        for line in f:
-            abc_arcos.append(line.split())
+        for line in lector:
+            if len(line) >= 2:
+                abc_arcos.append([line[0], line[1]])
 
-        for i in set_abc:
-            new_data = {i: counter}
-            dic.update(new_data)
+        for nodo in set_abc:
+            dic[nodo] = counter
             counter += 1
 
-    with open(index, 'w') as i:
+    with open(index_file, 'w') as i:
         i.write("Index  ID\n\n")
-        for key in dic.keys():
-            i.write(f"{dic[key]}      {key}\n")
+        for key, value in dic.items():
+            i.write(f"{value}      {key}\n")
 
-    n_nodos = len(abc_nodos)
+    n_nodos = len(set_abc)
     arcos_a = abc_arcos
 
     return dic
 
 
-def conversion(file: str, matrix: str, i_dic: dict) -> None:
+def conversion(file: str, matrix_file: str, i_dic: dict) -> None:
     final = []
 
-    with open(file, 'r') as f:
-        for i in arcos_a[1:len(arcos_a)]:
-            final.append([i_dic[i[0]], i_dic[i[1]]])
+    for arco in arcos_a:
+        if arco[0] in i_dic and arco[1] in i_dic:
+            final.append([i_dic[arco[0]], i_dic[arco[1]]])
 
-    with open(matrix, 'w') as m:
+    with open(matrix_file, 'w') as m:
         m.write(f"{n_nodos}\n")
         m.write(f"{len(final)}\n")
         for i in final:
             m.write(f"{i[0]}        {i[1]}\n")
 
-    return
 
 
 def run() -> None:
     ruta = './networks/weird'
     counter = 0
+
     for file in os.listdir(ruta):
         ruta_comp = os.path.join(ruta, file)
         if os.path.isfile(ruta_comp):
             i_dic = index(ruta_comp, f"./index/index{counter}.txt")
             conversion(ruta_comp, f"./networks/standards/matrix{counter}.txt", i_dic)
             counter += 1
-    return
