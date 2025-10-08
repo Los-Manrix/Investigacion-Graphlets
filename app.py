28bb6f96756c73b14d82b19435d2c5709c4adda3
Benjamin Cruz <bcruze@correo.uss.cl>
2025-10-07 23:12:53 -0300

    feat: incorporacion del tiempo de ejecucion

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import time
 from conversionFormato import conversion_formato as conv
 from nuevoOrdenamiento import Nuevo_Ordenamiento as new_ord
 
 def main():
+    start_time = time.time()
     conv.run()
     print("Conversion terminada...")
     new_ord.run()
     print("Ordenamiento terminado...")
     print("Proceso terminado")
+    end_time = time.time()
+    print(f"tiempo total: {end_time - start_time:.4f} segundos")
     return
 
 if __name__ == '__main__':
